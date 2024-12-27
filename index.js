92944af0d6afabc17f445ca1db54fb902846866b
Holly <162161116+Holl4444@users.noreply.github.com>
2024-12-27 23:23:59 +0000

    Add GET requests
    Added GET responses for / and /quotes.

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-
 // Set up express
 import express from 'express';
 const app = express();
-app.use(express.json);
-
+app.use(express.json());
 const PORT = 5000;
+
+import { getQuotes } from './helpers.js';
+
+app.get('/', function (req, res) {
+  res.send("Hi! How're you feeling?");
+});
+
+// Get all quotes
+app.get('/quotes', async function (req, res) {
+  const quotes = await getQuotes();
+  res.status(200).json(quotes);
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
