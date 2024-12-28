0594035f213735686e4766c6c06b81a75c2d99ea
Holly <162161116+Holl4444@users.noreply.github.com>
2024-12-28 18:55:51 +0000

    Update error handling

--- config.js
+++ config.js
@@ -2,15 +2,6 @@ import path from 'node:path';
 import { promises as fs } from 'fs';
 
 export const FILE_PATH = path.resolve(process.cwd(), 'quotes.json');
-// What happens in the code:
-// process.cwd() retrieves the current working directory where the Node.js script is being run.
-// 'quotes.json' is added to the path, and path.resolve() combines them into a full absolute path.
-// This full absolute path is assigned to the FILEPATH constant.
-// The FILEPATH constant is then exported so it can be used in other files/modules.
-// Example:
-// If the current working directory is /home/user/project, and 'quotes.json' is in the same directory, path.resolve() would return /home/user/project/quotes.json.
-
-// This line is essentially providing an absolute file path to the quotes.json file
 
 //Read file function
 //Make an async function
@@ -18,11 +9,15 @@ export async function readFile() {
   //try catch
   try {
     const quotes = await fs.readFile(FILE_PATH, 'utf8');
+    if (!quotes) {
+      console.log('File empty');
+      return [];
+    }
     return JSON.parse(quotes);
   } catch (err) {
     console.log(
       'Aye Carumba there was a problem reading the file',
-      err
+      err.message
     );
     return null;
   }
@@ -31,6 +26,9 @@ export async function readFile() {
 //Write file function
 export async function writeFile(quotes) {
   try {
+    if (!Array.isArray(quotes) || quotes.length === 0) {
+      throw new Error('Invalid data: must be a non-empty array.');
+    }
     await fs.writeFile(
       FILE_PATH,
       JSON.stringify(quotes, null, 2),
