8ab3a76e89dd084a9c039ca39b57dac1243f8cb4
Holly <162161116+Holl4444@users.noreply.github.com>
2024-12-28 00:56:15 +0000

    Add search by keyword
    Add search by keyword function
    
    Add return only author and quote. This will be the format we
    want to work with later and also cuts down the volume of data
    we are passing.

--- helpers.js
+++ helpers.js
@@ -1,8 +1,31 @@
 import { FILE_PATH } from './config.js';
 import { readFile, writeFile } from './config.js';
 
-
 export async function getQuotes() {
-    const quotes = await readFile();
-    return quotes;
+  const quotes = await readFile();
+  return quotes;
+}
+
+// Filter for quotes with tags that include the given keyword
+export async function getQuotesByKeyword(keyword) {
+  try {
+    const quotes = await getQuotes();
+    const keyQuotes = quotes.filter(
+      (quote) => quote.Tags && quote.Tags.includes(keyword)
+    );
+    return keyQuotes;
+  } catch (err) {
+    console.error('Error fetching quotes by keyword:', err);
+      return [];
+  }
+}
+
+// Filter out tags
+export function getQuoteAndAuthor(quotes) {
+  return quotes.map((quote) => {
+    return {
+      Quote: quote.Quote || "No quote Available",
+      Author: quote.Author || "Unknown",
+    };
+  });
 }
